83b3151c11f3624605f5ca99c833dd0d17e7f576
Riazantsev <riazantsevmark327@gmail.com>
2023-10-12 06:58:06 +0300

    Initial commit

---
+++ src/test/java/ru/netolgy/MobileBankApiTestV1.java
@@ -0,0 +1,29 @@
+package ru.netolgy;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("url", is("https://postman-echo.com/post"))
+                .contentType(ContentType.JSON)
+        ;
+    }
+
+}
